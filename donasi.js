586cfbc450e5575b2ef42ecd10f927e3d660dd1a
maulana907 <75834474+maulana907@users.noreply.github.com>
2021-02-16 02:32:36 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,21 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃       𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DINAI PAGGES ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ _+628888778625_
+┣━⊱ *PULSA*
+┣⊱ _+6289523731135_
+┣━⊱ *DANA*
+┣⊱ _08888778626
+┣━⊱ *GOPAY*
+┣⊱ _08888778626
+┣━⊱ *SAWERIA*
+┣⊱ _https://saweria.co/Maulanabhaktiar
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
